862618a3156b598e73f20e2bbf4020125635b224
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2020-09-04 15:31:21 -0300

    Avatar do deliveryman

---
+++ src/database/migrations/20200904180309-add-avatar-field-to-deliveryman.js
@@ -0,0 +1,15 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        return queryInterface.addColumn('deliverymans', 'avatar_id', {
+            type: Sequelize.INTEGER,
+            references: { model: 'files', key: 'id' },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+            allowNull: true,
+        });
+    },
+
+    down: async (queryInterface) => {
+        return queryInterface.removeColumn('deliverymans', 'avatar_id');
+    },
+};
