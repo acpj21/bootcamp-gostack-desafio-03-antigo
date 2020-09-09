78ad9277aa067afda81d0668676871db70b8a681
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2020-09-09 14:53:43 -0300

    Tabelas Delivery e DeliveryProblem

--- src/database/index.js
+++ src/database/index.js
@@ -4,10 +4,12 @@ import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
 import File from '../app/models/File';
 import Deliveryman from '../app/models/Deliveryman';
+import Delivery from '../app/models/Delivery';
+import DeliveryProblem from '../app/models/DeliveryProblem';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient, File, Deliveryman];
+const models = [User, Recipient, File, Deliveryman, Delivery, DeliveryProblem];
 
 class Database {
     constructor() {
